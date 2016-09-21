99f9ac27af5df363318629284b44992efb928175
Arjun Wadhwa <arjunw7@gmail.com>
2016-09-22 02:16:32 +0530

    upadate 1

---
+++ public/js/default.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".circle").delay(2000).animate({'width':'0px', 'height':'0px', 'margin-left':'0', 'margin-top': '0'}, 1000);
+	$(".line1").delay(5000).fadeOut();
+	$(".line2").delay(9500).fadeOut();
+	$(".line3").delay(14000).fadeOut();
+	$(".line4").delay(18500).fadeOut();
+	$(".line5").delay(25000).fadeOut();
+});
